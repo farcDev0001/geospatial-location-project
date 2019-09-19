f6b4368284a1c5a6f88ef1635551c2f050e2792c
Curro <farc@outlook.com>
2019-09-19 12:24:00 +0200

    update

--- modules/filterCitiesBar.py
+++ modules/filterCitiesBar.py
@@ -1,8 +1,18 @@
 def filterBar(city):
     from apiQueries import getJsonFourSquare
-    json = getJsonFourSquare(city['lat'],city['long'],'bar',10000)
+    json = getJsonFourSquare(city['lat'],city['long'],'bar',10000,lim=75)
     try:
         if len(json['response']['groups'][0]['items'])>70:
+            return True
+        return False
+    except BaseException():
+        return False
+
+def filterVegan(city):
+    from apiQueries import getJsonFourSquare
+    json = getJsonFourSquare(city['lat'],city['long'],'vegan',10000,lim=15)
+    try:
+        if len(json['response']['groups'][0]['items'])>10:
             return True
         return False
     except BaseException():
