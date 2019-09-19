92740f1ae68cd4f9d6378dc5ff01a945f9600efb
Curro <farc@outlook.com>
2019-09-19 12:24:47 +0200

    update

--- modules/filterCitiesSchool.py
+++ modules/filterCitiesSchool.py
@@ -1,6 +1,6 @@
 def filterUniversity(city):
     from apiQueries import getJsonFourSquare
-    json = getJsonFourSquare(city['lat'],city['long'],'university',10000)
+    json = getJsonFourSquare(city['lat'],city['long'],'university',10000,lim = 15)
     if len(json['response']['groups'][0]['items'])>6:
         return True
     return False
