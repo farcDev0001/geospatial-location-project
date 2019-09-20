46eb5af94aa27e015f661504f2788bb144a38748
farc@outlook.es <farc@outlook.es>
2019-09-20 18:19:03 +0200

    families n vegan cities

--- modules/filterCitiesBar.py
+++ modules/filterCitiesBar.py
@@ -1,19 +1,19 @@
 def filterBar(city):
     from apiQueries import getJsonFourSquare
-    json = getJsonFourSquare(city['lat'],city['long'],'bar',10000,lim=75)
+    json = getJsonFourSquare(city['lat'],city['long'],'bar',10000,lim=100)
     try:
-        if len(json['response']['groups'][0]['items'])>70:
+        if len(json['response']['groups'][0]['items'])>90:
             return True
         return False
-    except BaseException():
+    except Exception():
         return False
 
 def filterVegan(city):
     from apiQueries import getJsonFourSquare
-    json = getJsonFourSquare(city['lat'],city['long'],'vegan',10000,lim=15)
+    json = getJsonFourSquare(city['lat'],city['long'],'vegan',10000,lim=60)
     try:
-        if len(json['response']['groups'][0]['items'])>10:
+        if len(json['response']['groups'][0]['items'])>55:
             return True
         return False
-    except BaseException():
+    except Exception():
         return False
