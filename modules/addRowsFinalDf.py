c1c60f0af7656e2c6d06e0cb80ddec675ee7bfd8
farc@outlook.es <farc@outlook.es>
2019-09-22 21:53:58 +0200

    Done

---
+++ modules/addRowsFinalDf.py
@@ -0,0 +1,21 @@
+def addByQuery(df,lat,long,query,distance = 10000):
+    from apiQueries import getJsonFourSquare
+    json = getJsonFourSquare(lat,long,query,distance,lim=50)
+    for ele in json['response']['groups'][0]['items']:
+        category = query
+        name = ele['venue']['name']
+        lat = ele['venue']['location']['lat']
+        long = ele['venue']['location']['lng']
+        df = df.append({'name':name,'category':category,'lat':lat,'long':long},ignore_index=True)
+    return df
+
+def addRows(df,lat,long):
+    df = (addByQuery(df,lat,long,'Starbucks'))
+    df = (addByQuery(df,lat,long,'University',distance=25000))
+    df = (addByQuery(df,lat,long,'Bar'))
+    df = (addByQuery(df,lat,long,'Vegan'))
+    df = (addByQuery(df,lat,long,'Airport',distance=25000))
+    df = (addByQuery(df,lat,long,'Bus Station'))
+    df = (addByQuery(df,lat,long,'Museum'))
+    df = (addByQuery(df,lat,long,'76ers'))
+    return df
---
+++ modules/getPoint.py
@@ -0,0 +1,36 @@
+def officePhilaOk():
+    from pymongo import MongoClient
+    import pandas as pd
+    from getMongoClient import getClient
+    from getEnv import getVariable
+    client = getClient()
+    dbCompanies = client.companies
+    cambridge1m = list(dbCompanies.philadelphia.find())
+    listCambridge = getListOffice2km(cambridge1m,dbCompanies)
+
+
+    return listCambridge
+
+
+def getCompaniesNear(lat,long, max_Distance,db):
+    return list(db.cambridge.find({
+        "position": {
+            "$near": {
+               "$geometry": {
+                  "type": "Point" ,
+                  "coordinates": [ long , lat ]
+               },
+               "$maxDistance": max_Distance
+               
+             }
+       }
+    }))
+
+def getListOffice2km(listOffice,db):
+    import pandas as pd
+    listDf = []
+    for ele in listOffice:
+        if ele['founded_year'] > 2008:
+            df = pd.DataFrame(getCompaniesNear(ele['lat'],ele['long'],2000,db))
+            listDf.append({'office':ele,'df':df.reset_index().drop(columns='index')})
+    return listDf
